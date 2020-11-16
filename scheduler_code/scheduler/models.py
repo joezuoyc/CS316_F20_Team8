76ee64587f663946df59d116cd6ecced55bed946
David Wu <ruoyu.wu@duke.edu>
2020-11-16 11:00:23 +0800

    allow for more characters in username

--- scheduler_code/scheduler/models.py
+++ scheduler_code/scheduler/models.py
@@ -13,7 +13,7 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     is_manager = db.Column(db.Boolean, nullable=False)
     dept = db.Column(db.String(60), nullable=False)
